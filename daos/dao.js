6a0758d1772cf79a97eccda2ee4eda0793fb3d71
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-11-05 11:30:25 -0600

    pushing code
    updating files. Dao.js needed updating

--- daos/dao.js
+++ daos/dao.js
@@ -3,16 +3,26 @@ const daoCommon = require('./common/daoCommon')
 const albumDao = {
     ...daoCommon,
     ...require('./api/albumDao')
-    
 }
 
 const artistDao = {
     ...daoCommon,
     ...require('./api/artistDao')
-    
+}
+
+const bandDao = {
+    ...daoCommon,
+    ...require('./api/bandDao')
+}
+
+const labelDao = {
+    ...daoCommon,
+    ...require('./api/labelDao')
 }
 
 module.exports = {
     albumDao,
-    artistDao
+    artistDao,
+    bandDao,
+    labelDao
 }
--- routes/api/bandRoutes.js
+++ routes/api/bandRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const { bandDao : dao } = require('../../daos/dao')
 
-//? http://localhost:3000/api/band
+// http://localhost:3000/api/band
 router.get('/', (req, res)=> {
     dao.findAll(req, res, dao.table)
 })
@@ -11,7 +11,7 @@ router.get('/get_albums/:id', (req, res)=> {
     dao.findAlbumsByBand(res, dao.table, req.params.id)
 })
 
-//? http://locahost:3000/api/band/sort/:sort
+// http://locahost:3000/api/band/sort/:sort
 router.get('/sort/:sorter', (req, res)=> {
     dao.sort(res, dao.table, req.params.sorter)
 })
